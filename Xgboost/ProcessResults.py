4f731deb9f5cf50d6b8dee2bdc23a3b7c8e94cae
RuggerDog15 <127642320+RuggerDog15@users.noreply.github.com>
2023-03-25 18:56:19 -0400

    Add files via upload

---
+++ Xgboost/ProcessResults.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 14 18:22:29 2023
+
+@author: mfloy
+"""
+
+import os
+from pathlib import Path
+import warnings
+from tqdm import tqdm
+import pandas as pd
+warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
+import time
+import datetime
+
+w_factor = .05
+dataIn = Path("./Data")
+rt = dataIn / "results.csv"
+results = pd.read_csv(rt)
+
+home_score =  (results["home_score"] - results["away_score"]).tolist()
+
+home_loss = []
+home_draw = []
+home_win = []
+
+for X in home_score:
+    if X > 0:
+        home_win.append(1)
+    else:
+        home_win.append(0)
+        
+for X in home_score:
+    if X == 0:
+        home_draw.append(1)
+    else:
+        home_draw.append(0)
+        
+for X in home_score:
+    if X < 0:
+        home_loss.append(1)
+    else:
+        home_loss.append(0)
+
+results["home_win"] = home_win
+results["home_loss"] = home_loss
+results["home_draw"] = home_draw
+
+results["year"] = pd.DatetimeIndex(results["date"]).year
+results["weight_date"] = (1 + ((results["year"]-1872) * w_factor))
+
+dataOut = Path("./Data")
+
+rout = os.path.join(dataOut, "results-out.csv")
+results.to_csv(rout, index=False)
+
+#torns = results["tournament"].drop_duplicates()
+
+torns = pd.read_csv(dataOut / "torns-out.csv")
+
+results_torn = torns.merge(results, on="tournament")
+
+missingRaw = results.loc[results_torn["Raw"] == ""]
+
+results_torn["weight"] =  (results_torn["Modified"] * results_torn["weight_date"])
+
+results_torn.to_csv(dataOut / "results_torn.csv", index=False)
+
+#create initial elo table
---
+++ Xgboost/TrainGames.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 14 18:22:29 2023
+
+@author: mfloy
+"""
+
+from pathlib import Path
+import warnings
+from tqdm import tqdm
+import pandas as pd
+warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
+import time
+
+datafolder = Path("./Data/")
+rt = datafolder / "results_elo.csv"
+results = pd.read_csv(rt)
+re = datafolder / "team_elo.csv"
+elo_teams = pd.read_csv(re)
+
+import xgboost
+
+fx = ["home_team", "away_team", "home_elo", "away_elo", "neutral", "Raw"]
+#fx = ["home_elo", "away_elo"]
+fy = ["home_win", "home_loss", "home_draw"]
+#fy = ["home_win", "home_loss"]
+#fy = ["home_score", "away_score"]
+snum = 0
+weight = results["weight"]
+weight = weight.loc[snum:]
+weight = weight.to_list()
+
+
+X = results[(fx)]
+Y = results[(fy)]
+X = X.loc[snum:]
+Y = Y.loc[snum:]
+
+X["home_team"] = X["home_team"].astype("category")
+X["away_team"] = X["away_team"].astype("category")
+
+start_proc, start_time = time.process_time(), time.perf_counter()
+
+Y_hat = pd.DataFrame()
+models = {}
+for col in tqdm(list(Y.columns)):
+    model = xgboost.XGBClassifier(tree_method="gpu_hist", n_estimators=32, max_depth=32, eval_metric='logloss', enable_categorical=True, max_cat_to_onehot=1)
+    model.fit(X, Y[col], sample_weight=weight)
+    #model.fit(X, Y[col])
+    models[col] = model
+    model.save_model(datafolder / f"model-{col}.json")
+    
+# models.save_model(datafolder / "model-cat.json")
+    
+stop_proc, stop_time = time.process_time(), time.perf_counter()
+
+print("Process time ", stop_proc-start_proc, "  Elapsed Time ", stop_time-start_time)    
+    
+from sklearn.metrics import brier_score_loss, roc_auc_score, log_loss
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.model_selection import train_test_split
+
+testX, testY = X, Y
+
+xtrain, xtest, ytrain, ytest=train_test_split(X, Y, test_size=0.15)
+
+def evaluate(y, y_hat):
+    p = sum(y) / len(y)
+    base = [p] * len(y)
+    brier = brier_score_loss(y, y_hat)
+    print(f"  Brier score: %.5f (%.5f)" % (brier, brier / brier_score_loss(y, base)))
+    ll = log_loss(y, y_hat)
+    print(f"  log loss score: %.5f (%.5f)" % (ll, ll / log_loss(y, base)))
+    print(f"  ROC AUC: %.5f" % roc_auc_score(y, y_hat))
+
+start_proc, start_time = time.process_time(), time.perf_counter()
+
+for col in testY.columns:
+    Y_hat[col] = [p[1] for p in models[col].predict_proba(testX)]
+    print(f"### Y: {col} ###")
+    evaluate(testY[col], Y_hat[col])
+    cv_score = cross_val_score(model, xtrain, ytrain, cv=5)
+    print("  Mean cross-validation score: %.2f" % cv_score.mean())
+    kfold = KFold(n_splits=10, shuffle=True)
+    kf_cv_scores = cross_val_score(model, xtrain, ytrain, cv=kfold)
+    print("  K-fold CV average score: %.2f" % kf_cv_scores.mean())
+    
+stop_proc, stop_time = time.process_time(), time.perf_counter()
+print("Process time ", stop_proc-start_proc, "  Elapsed Time ", stop_time-start_time)   
+
+hometeam = "Bermuda"
+awayteam = "Guyana"
+elo_home = elo_teams.loc[(elo_teams["Team"] == hometeam), "ELOs"].item()
+elo_away = elo_teams.loc[(elo_teams["Team"] == awayteam), "ELOs"].item()
+
+teams = {"home_team": [hometeam], "away_team": [awayteam],"home_elo": [elo_home], "away_elo": [elo_away], "neutral": False, "Raw": 40}
+#teams = {"home_elo": [elo_home], "away_elo": [elo_away]}
+
+GameX = pd.DataFrame(teams)
+
+GameX["home_team"] = GameX["home_team"].astype("category")
+GameX["away_team"] = GameX["away_team"].astype("category")
+P_hat =pd.DataFrame()
+
+for col in testY.columns:  
+    P_hat[col] = [p[1] for p in models[col].predict_proba(GameX)] 
+    
